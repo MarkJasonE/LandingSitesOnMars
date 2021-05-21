4b8acd2255fe5ed7a8b04442cc89628436d56f5c
JasonEgipto <jasonegipto@gmail.com>
2021-05-21 17:34:06 +0300

    Added search class. Definied within it: run_rect_stats

--- landing.py
+++ landing.py
@@ -3,7 +3,7 @@ import numpy as np
 import tkinter as tk
 from PIL import Image, ImageTk
 
-#CONSTANTS
+#CONSTANTS: Inputs
 IMG_GRAY = cv.imread("images/mola_1024x501.png", cv.IMREAD_GRAYSCALE)
 IMG_COLOR = cv.imread("images/mola_color_1024x506.png")
 RECT_WIDTH_KM = 670
@@ -13,7 +13,6 @@ NUM_CANDIDATES = 20
 MARS_CIRCUM = 21344
 
 #CONSTANTS: Derived
-
 IMG_HT, IMG_WIDTH = IMG_GRAY.shape #(501(# px in rows), 1024(# px in columns))
 PIXELS_PER_KM = IMG_WIDTH / MARS_CIRCUM #Convert dims from km to px
 RECT_WIDTH = int(PIXELS_PER_KM * RECT_WIDTH_KM) #Convert width to px
@@ -26,3 +25,46 @@ STEP_Y = int(RECT_HT / 2) #How far our rectangle box will move on the Y axis
 #Initialize tkinter screen and draw on canvas
 screen = tk.Tk()
 canvas = tk.Canvas(screen, width=IMG_WIDTH, height=IMG_HT + 130)
+
+#Search
+class Search():
+    """ Read the image and identify landing rectangles based on requirement inputs. """
+    
+    def __init__(self, name):
+        self.name = name
+        self.rect_coords = {} #Corner points
+        self.rect_means = {} #Mean elevation
+        self.rect_ptps = {} #Peak to valley, to be consistent with Numpy, we'll use peak to peak
+        self.rect_stds = {} #Standard deviation
+        self.ptp_filtered = []
+        self.std_filtered = []
+        self.high_graded_rects = [] #Lowest combined score
+
+    def run_rect_stats(self):
+        """ Define rectangular search areas and calculate internal stats. """
+        ul_x, ul_y = 0, LAT_30_N #Upper left, X and Y coords
+        lr_x, lr_y = RECT_WIDTH, LAT_30_N + RECT_HT #Lower right, X and Y coords
+        rect_num = 1 #Keep track of rects
+
+        while True:
+            rect_img = IMG_GRAY[ul_y : lr_y, ul_x : lr_x] #From the whole image, slice only the relevant rect
+            self.rect_coords[rect_num] = [ul_x, ul_y, lr_x, lr_y] #Store coords into dictionary
+
+            if np.mean(rect_img) <= MAX_ELEV_LIMIT:
+                self.rect_means[rect_num] = np.mean(rect_img)
+                self.rect_ptps[rect_num] = np.ptp(rect_img)
+                self.rect_stds[rect_num] = np.std(rect_img)
+            rect_num += 1
+
+            #Move rect
+            ul_x += STEP_X
+            lr_x = ul_x + RECT_WIDTH
+            #If rect extends past the right side of whole image, reset X coord to 0 and move to a new Y row
+            if lr_x > IMG_WIDTH:
+                ul_x = 0
+                ul_y += STEP_Y
+                lr_x = RECT_WIDTH
+                lr_y += STEP_Y
+            if lr_y > LAT_30_S + STEP_Y:
+                #Stop if more than half of rect extend below latitude 30 south
+                break
